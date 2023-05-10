eb3f8d640a1d28ba89bb12c399a1b731b2868d0f
Jannik Jost <jannik-jost@gmx.de>
2023-05-10 19:47:39 +0200

    Improvements
    -Clean directory at the start
    -Fixed duplicated title, url

--- raindrop.js
+++ raindrop.js
@@ -1,37 +1,81 @@
 const axios = require("axios");
 const token = require("./token.json");
-const fs = require("fs");
+const fsPromises = require("fs/promises");
+const path = require("path");
 
 const url = "https://api.raindrop.io/rest/v1/highlights";
 
-axios
-  .get(url, {
-    headers: {
-      Authorization: "Bearer " + token.token,
-    },
-  })
-  .then((res) => {
-    console.log("res", res.data);
-    if (!res.data) return;
-    res.data.items.forEach((element) => {
-      const fileText = generateNoteForHighlight(element);
-      writeNote(element.title, fileText);
+cleanDirectory("./highlightsToSync").then(() => {
+  axios
+    .get(url, {
+      headers: {
+        Authorization: "Bearer " + token.token,
+      },
+    })
+    .then(async (res) => {
+      if (!res.data) return;
+      for (const element of res.data.items) {
+        await writeNote(element.title, element);
+      }
+      await addCurlyBracket();
+    })
+    .catch((err) => {
+      console.log("Error: ", err.message);
     });
-  })
-  .catch((err) => {
-    console.log("Error: ", err.message);
-  });
+});
 
-function generateNoteForHighlight(highlight) {
-  let note = "\b\fs28" + highlight.title;
-  note += "\\par\\i " + highlight.link;
-  note += "\\par " + highlight.text;
-  return note;
+async function writeNote(filename, element) {
+  await writeFileWithHeader(filename, element);
+  await fsPromises.appendFile(
+    `./highlightsToSync/${filename}.rtf`,
+    generateNoteForHighlight(element),
+    function (err) {
+      if (err) throw err;
+    }
+  );
 }
 
-function writeNote(filename, note) {
-  fs.appendFile(`./highlightsToSync/${filename}.rtf`, note, function (err) {
-    if (err) throw err;
-    console.log("Saved!");
-  });
+async function writeFileWithHeader(filename, element) {
+  const files = await fsPromises.readdir("./highlightsToSync");
+  if (!files.includes(filename + ".rtf")) {
+    await fsPromises.writeFile(
+      `./highlightsToSync/${filename}.rtf`,
+      generateFileHeader(element)
+    );
+  }
+}
+
+function generateFileHeader(element) {
+  let header = "{\\rtf1 ";
+  header += element.title + " \\line ";
+  header += "{\\i " + element.link + "}";
+  return header;
+}
+
+function generateNoteForHighlight(highlight) {
+  return " \\line " + highlight.text;
+}
+
+async function cleanDirectory(folderPath) {
+  try {
+    const files = await fsPromises.readdir(folderPath);
+    for (const file of files) {
+      await fsPromises.unlink(path.resolve(folderPath, file));
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function addCurlyBracket() {
+  const files = await fsPromises.readdir("./highlightsToSync");
+  for (const file of files) {
+    await fsPromises.appendFile(
+      path.resolve("./highlightsToSync", file),
+      "}",
+      function (err) {
+        if (err) throw err;
+      }
+    );
+  }
 }
